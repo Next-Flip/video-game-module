b62abed132a624d112c0d281290f329b148a75e6
WillyJL <49810075+WillyJL@users.noreply.github.com>
2025-06-01 04:42:51 +0100

    Revert "Increase communication baud rate"
    This reverts commit c44ad25097b869b371aa1750051e1c371a0e5167.

--- app/uart.c
+++ app/uart.c
@@ -23,7 +23,7 @@
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
 #define UART_INIT_BAUD_RATE (9600UL)
-#define UART_BAUD_RATE (1843200UL)
+#define UART_BAUD_RATE (921600UL)
 
 #define EXPANSION_MODULE_TIMEOUT_MS (EXPANSION_PROTOCOL_TIMEOUT_MS - 50UL)
 #define EXPANSION_MODULE_STARTUP_DELAY_MS (250UL)
@@ -34,14 +34,12 @@ static PB_Main rpc_message;
 
 // RX interrupt handler
 static void uart_on_rx() {
-    BaseType_t higher_priority_task_woken = pdFALSE;
-
     while(uart_is_readable(UART_ID)) {
-        const uint8_t ch = uart_getc(UART_ID);
-        xStreamBufferSendFromISR(stream, &ch, sizeof(ch), &higher_priority_task_woken);
+        uint8_t ch = uart_getc(UART_ID);
+        BaseType_t pxHigherPriorityTaskWoken;
+        xStreamBufferSendFromISR(stream, &ch, sizeof(ch), &pxHigherPriorityTaskWoken);
+        portYIELD_FROM_ISR(pxHigherPriorityTaskWoken);
     }
-
-    portYIELD_FROM_ISR(higher_priority_task_woken);
 }
 
 // Receive frames
